2942f13b86f7ca196be9c13520b22b97e179791b
Robert DeLuca <robertdeluca19@gmail.com>
2019-09-20 15:52:07 -0500

    Setup repo for initial alpha release

---
+++ percy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Perceptual Inc.'
+__email__ = 'team@percy.io'
+__version__ = '0.1.0'
+
+from percy.snapshot import percySnapshot
---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read()
+
+setuptools.setup(
+    name='percy-python-selenium',
+    version='0.1.0',
+    description='Python client for visual regression testing with Percy (https://percy.io).',
+    author='Perceptual Inc.',
+    author_email='team@percy.io',
+    url='https://github.com/percy/percy-python-selenium',
+    packages=[
+        'percy',
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    keywords='percy',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=['selenium']
+)
