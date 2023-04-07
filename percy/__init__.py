f0b5d5597eac954214d64f2e5ef55645f5a69819
ninadbstack <60422475+ninadbstack@users.noreply.github.com>
2023-04-07 11:38:48 +0530

    Updated module loader to support both packages
    Now we support `percy-selenium` and `percy-appium-app` in same python environment
    By importing both packages in both __init__ files, even though one overrides other while installation, either can be used to execute both commands as long as packages are installed

--- percy/__init__.py
+++ percy/__init__.py
@@ -1,6 +1,20 @@
-from percy.snapshot import percy_snapshot
 from percy.version import __version__
 
+# import snapshot command
+try:
+  from percy.snapshot import percy_snapshot
+except:
+  def percy_snapshot(driver, *a, **kw):
+    raise Exception("[percy] `percy-selenium` package is not installed, please install it to use percy_snapshot command")
+  
 # for better backwards compatibility
 def percySnapshot(browser, *a, **kw):
-    return percy_snapshot(driver=browser, *a, **kw)
+  return percy_snapshot(driver=browser, *a, **kw)
+
+# improt screenshot command
+try:
+  from percy.screenshot import percy_screenshot
+except:
+  def percy_screenshot(driver, *a, **kw):
+    raise Exception("[percy] `percy-appium-app` package is not installed, please install it to use percy_screenshot command")
+
